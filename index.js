cfedd570199bdc431bb6695e94c004a615a2b826
yness <daudayohanna093@gmail.com>
2019-09-11 13:59:37 +0100

    My First Test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+var div=document.getElementById("hello");
+div.style.backgroundColor="#0045";
+div.style.height="100px";
+div.style.width="200px";
+div.style.padding="50px 50px";
+div.style.borderRadius="5px 20000px"
+div.style.boxShadow="grey 5px 5px 5px";
+var p = document.createElement("p");
+p.style.color="red";
+p.style.fontStyle.fontsize="italic";
+var text = "Java Script is really interesting";
+p.innerHTML=text;
+div.appendChild(p)
